66a002a608ea99a3bea54a60795c9fd531445371
anj501 <110501128+anj501@users.noreply.github.com>
2024-11-21 21:58:11 +0000

    finished backward elimination

--- Project2Part1/main.py
+++ Project2Part1/main.py
@@ -13,7 +13,7 @@ class Feature:
         for i in range(1, self.numFeatures + 1):
             if i not in currFeature:
                 tempFeature = list(currFeature) + [i]
-                accuracy = round(random.uniform(20, 100))
+                accuracy = round(random.uniform(1, 100))
                 print("Using feature(s) " + str(set(tempFeature)) + " accuracy is " + str(accuracy) + "%")
                 if accuracy > bestAccuracy:
                     bestAccuracy = accuracy
@@ -27,7 +27,33 @@ class Feature:
                 bestFeature = list(currFeature)
             
     print("Finished search!! The best feature subset is " + str(set(bestFeature)) + ", which has an accuracy of " + str(overallAccuracy) + "%")
+ 
+ def backwardElimination(self):
+    currFeature = list(range(1, self.numFeatures + 1))
+    overallAccuracy = 0
+    bestFeature = list(currFeature)
 
+    while len(currFeature) > 0:
+        removeFeature = 0
+        bestAccuracy = 0
+
+        for i in currFeature:
+            tempFeature = currFeature.copy()
+            tempFeature.remove(i)
+            accuracy = round(random.uniform(1, 100))
+            print("Using feature(s) " + str(set(tempFeature)) + " accuracy is " + str(accuracy) + "%")
+            if accuracy > bestAccuracy:
+                bestAccuracy = accuracy
+                removeFeature = i
+            
+        if removeFeature in currFeature:
+                currFeature.remove(removeFeature)
+                print("Feature set " + str(set(currFeature)) + " was best, accuracy is " + str(bestAccuracy) + "%")
+                if bestAccuracy > overallAccuracy:
+                    overallAccuracy = bestAccuracy
+                    bestFeature = list(currFeature)
+        
+    print("Finished search!! The best feature subset is " + str(set(bestFeature)) + ", which has an accuracy of " + str(overallAccuracy) + "%")
 def main():
     print("Welcome to Justin's and Kyle's Feature Selection Algorithm.")
     numFeatures = int(input("Please enter the total number of features: "))
@@ -38,6 +64,8 @@ def main():
 
     if choice == 1:
      featAlgor.fowardSelect()
+    elif choice == 2:
+     featAlgor.backwardElimination()
 
 if __name__ == "__main__":
     main()
