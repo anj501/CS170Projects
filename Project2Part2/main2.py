a771d5333f366c0a6a9e6b328c8dc3de755396e2
Kyle Taing <kyletaing@Kyles-MacBook-Air-3.local>
2024-12-05 16:28:59 -0800

    Finish Classfier class

--- Project2Part2/main2.py
+++ Project2Part2/main2.py
@@ -1,19 +1,44 @@
+from math import sqrt
+
 class Classfier:
     def __init__(self):
         self.trainData = None
 
     def training(self, data):
         self.trainData = data
-    
+
+    def test(self, testInstance, featIndice):
+
+        trainFeat = []
+        for i in self.trainData:
+            trainFeat.append([i[j] for j in featIndice])
+
+        testFeat = []
+        for i in featIndice:
+            testFeat.append(testInstance[i])
+        
+        Edistance = []
+        for i in trainFeat:
+            distance = 0
+            for j in range(len(i)):
+                distance += (i[j] - testFeat[j]) ** 2
+            Edistance.append(sqrt(distance))
+        
+        nearestNeighbor = Edistance.index(min(Edistance))
+        return self.trainData[nearestNeighbor][0]  
+
 class Validator:
     def __init__(self, classfier):
         self.classfier = classfier
+
+    
         
+
 def loadData(filePath):
     data = []
     with open(filePath, 'r') as file:
-        for i in file:
-            values = (map(float, i.split()))
+        for line in file:
+            values = list(map(float, line.split()))
             data.append(values)
     return data
 
@@ -27,5 +52,5 @@ if __name__ == "__main__":
         exit()
 
     
-    
-    
+    featSubset = list(map(int, input("Enter the feature subset: ").split()))  
+
